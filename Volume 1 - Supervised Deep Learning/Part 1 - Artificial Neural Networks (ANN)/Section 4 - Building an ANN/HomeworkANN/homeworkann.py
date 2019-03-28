e63f6e2cd7baa11bf67ed12b73a2713d43987768
Doğukan <10725670+Dogukan189@users.noreply.github.com>
2019-03-28 15:43:22 +0300

    Update homeworkann.py

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/HomeworkANN/homeworkann.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/HomeworkANN/homeworkann.py
@@ -6,3 +6,31 @@ x = dataset.iloc[:,1:4].values
 y = dataset.iloc[:,-1].values
 
 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.fit(x_test)
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()
+
+classifier.add(Dense(output_dim = 2, init="uniform", activation="relu",input_dim=4))
+
+classifier.add(Dense(output_dim = 2, init="uniform", activation="relu"))
+
+
+
+
+
+
+
