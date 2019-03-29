675321cd771d64987d267291571b9d7d2b7564a8
Doğukan <10725670+Dogukan189@users.noreply.github.com>
2019-03-29 20:21:14 +0300

    completede homework

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/HomeworkANN/homeworkann.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/HomeworkANN/homeworkann.py
@@ -1,34 +1,73 @@
 import numpy as np
 import pandas as pd
 
-dataset = pd.read_csv("Iris.csv")
-x = dataset.iloc[:,1:4].values
-y = dataset.iloc[:,-1].values
+dataset = pd.read_csv("heart.csv")
+dataset = dataset.iloc[:300,:]
+x = dataset.iloc[:,0:13].values
+y = dataset.iloc[:,13].values
 
-#%%
+
+
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelEncoder_y = LabelEncoder()
-y = labelEncoder_y.fit_transform(y)
+sexEncoder = OneHotEncoder(categorical_features = [1])
+cpEncoder = OneHotEncoder(categorical_features = [2])
+fbsEncoder = OneHotEncoder(categorical_features = [5])
+restecgEncoder = OneHotEncoder(categorical_features = [6])
+exangEncoder = OneHotEncoder(categorical_features = [8])
+slopeEncoder = OneHotEncoder(categorical_features = [10])
+caEncoder = OneHotEncoder(categorical_features = [11])
+thalEncoder = OneHotEncoder(categorical_features = [12])
+x = sexEncoder.fit_transform(x).toarray()
+x = cpEncoder.fit_transform(x).toarray()
+x = fbsEncoder.fit_transform(x).toarray()
+x = restecgEncoder.fit_transform(x).toarray()
+x = exangEncoder.fit_transform(x).toarray()
+x = slopeEncoder.fit_transform(x).toarray()
+x = caEncoder.fit_transform(x).toarray()
+x = thalEncoder.fit_transform(x).toarray()
 
+# Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
-x_test = scaler.fit(x_test)
+x_test = scaler.transform(x_test)
 
-import keras
-from keras.models import Sequential
+import keras 
 from keras.layers import Dense
+from keras.models import Sequential
 
 classifier = Sequential()
 
-classifier.add(Dense(output_dim = 2, init="uniform", activation="relu",input_dim=4))
+classifier.add(Dense(output_dim=8,activation="relu",init="uniform",input_dim=60))
 
-classifier.add(Dense(output_dim = 2, init="uniform", activation="relu"))
+classifier.add(Dense(output_dim=8,activation="relu",init="uniform",))
 
-classifier.add(Dense(output_dim = 1 ,init="uniform", activation))
+classifier.add(Dense(output_dim=8,activation="relu",init="uniform",))
+
+classifier.add(Dense(output_dim=1,activation="sigmoid",init="uniform"))
+
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+#Fitting the ANN to the Training Set
+classifier.fit(x_train,y_train, batch_size = 10,nb_epoch = 1000)
+
+y_pred = classifier.predict(x_test)
+y_pred_acc = (y_pred > 0.5)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred_acc)
+
+
+
+
+
+
+
+
+
 
 
 
--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -11,6 +11,7 @@ X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 df = pd.DataFrame(X)
 
+
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
 X[:,1] = labelencoder_X_1.fit_transform(X[:,1])
@@ -19,6 +20,7 @@ X[:,2] = labelencoder_X_2.fit_transform(X[:,2])
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
+
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
