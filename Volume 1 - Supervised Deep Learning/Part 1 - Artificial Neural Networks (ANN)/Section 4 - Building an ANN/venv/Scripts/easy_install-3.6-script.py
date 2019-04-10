e6d3d6a6ed3a80c06bc3549e68cbdaa4fe4f624a
Doğukan <10725670+Dogukan189@users.noreply.github.com>
2019-04-10 14:45:01 +0300

    asdf

---
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/venv/Scripts/easy_install-3.6-script.py
@@ -0,0 +1,12 @@
+#!"D:\Dogukan\Documents\GitHub\Dogukan-DL-keras\Volume 1 - Supervised Deep Learning\Part 1 - Artificial Neural Networks (ANN)\Section 4 - Building an ANN\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.6'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install-3.6')()
+    )
