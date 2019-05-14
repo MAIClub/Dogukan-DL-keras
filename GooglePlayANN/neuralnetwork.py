e8f5172a7442d7b30e1594ddd66be215bf8fbed8
Dogu Kan <dogukan@localhost.localdomain>
2019-05-15 00:34:25 +0300

    completed googleplaynn

--- GooglePlayANN/neuralnetwork.py
+++ GooglePlayANN/neuralnetwork.py
@@ -1,11 +1,3 @@
-with open("googleplaystore.csv", "r") as f:
-    lines = f.readlines()
-with open("googleplaystore.csv", "w") as f:
-    for line in lines:
-        if "nan" not in line:
-            f.write(line)
-
-#%%
             
 import pandas as pd
 import numpy as np
@@ -25,7 +17,7 @@ x = df.iloc[:,1:5].join(df.iloc[:,6:10]).values
 y = df["Installs"].values
 #Label and Hot Encode
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
-
+from keras.utils import np_utils
 categoryEncoder = LabelEncoder()
 typeEncoder = LabelEncoder()
 contentEncoder = LabelEncoder()
@@ -36,29 +28,71 @@ x[:,6] = contentEncoder.fit_transform(x[:,6])
 x[:,7] = genreEncoder.fit_transform(x[:,7])
 oneHotEncoder = OneHotEncoder(categorical_features=[0,4,6,7])
 x = oneHotEncoder.fit_transform(x).toarray()
-yencoder = LabelEncoder()
-y =  yencoder.fit_transform(y)
+#
+# encode class values as integers
+encoder = LabelEncoder()
+encoder.fit(y)
+encoded_Y = encoder.transform(y)
+# convert integers to dummy variables (i.e. one hot encoded)
+dummy_y = np_utils.to_categorical(encoded_Y)
+
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.2,random_state = 0)
+x_train,x_test,y_train,y_test = train_test_split(x,dummy_y, test_size=0.2,random_state = 0)
 
 from sklearn.preprocessing import StandardScaler
 scaler=StandardScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-#%%
+
 from keras.models import Sequential
 from keras.layers import Dense
-
+#%% NORMAL TRAIN
 
 
 model =Sequential()
-model.add(Dense(units = 12, init = 'uniform', activation = 'softmax', input_dim = 155))
-model.add(Dense(units = 12, init = 'uniform', activation = 'softmax'))
-model.add(Dense(units = 1, init = 'uniform', activation = 'softmax'))
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+model.add(Dense(units = 78, init = 'uniform', activation = 'softsign', input_dim = 155))
+model.add(Dense(units = 78, init = 'uniform', activation = 'softsign'))
+model.add(Dense(units = 19, init = 'uniform', activation = 'softmax'))
+model.compile(optimizer = 'adamax', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 model.fit(x_train, y_train, batch_size = 10, epochs = 100)
+#%%CROSS VAL
 
+def model():
+    classifier = Sequential()
+    classifier.add(Dense(units=78, init='uniform', activation='tanh', input_dim=155))
+    classifier.add(Dense(units=78, init='uniform', activation='tanh'))
+    classifier.add(Dense(units=19, init='uniform', activation='sigmoid'))
+    classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return classifier
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+classifier = KerasClassifier(build_fn=model, batch_size=10, epochs=500)
+accuracies = cross_val_score(estimator=classifier, X=x_train, y=y_train, cv=10, n_jobs=-1)
+variance = accuracies.std()
+mean = accuracies.mean()
+
+print(mean, variance)
+#%% GRIDSEARCH
+
+def model(activation):
+    classifier = Sequential()
+    classifier.add(Dense(units=78, init='uniform', activation=activation, input_dim=155))
+    classifier.add(Dense(units=78, init='uniform', activation=activation))
+    classifier.add(Dense(units=19, init='uniform', activation='softmax'))
+    classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return classifier
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+
+classifier = KerasClassifier(build_fn=model)
+parameters = {'batch_size' : [10,25],'epochs' : [500,750], 'activation' : ['relu','softsign']}
+grid_search = GridSearchCV(estimator = classifier, param_grid = parameters, scoring = "accuracy",cv = 10)
+grid_search = grid_search.fit(x_train,y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
 
 
 
