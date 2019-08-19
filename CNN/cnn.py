7fea0749c43217f59064b488356397663dec04de
Dogu Kan <dogukan@localhost.localdomain>
2019-08-19 22:07:21 +0300

    val_loss: 0.3919 - val_acc: 0.8545

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -1,14 +1,24 @@
 
 #%%
 from keras.models import Sequential
-from keras.layers import Convolution2D,MaxPooling2D,Flatten,Dense
+from keras.layers import Convolution2D,MaxPooling2D,Flatten,Dense,Dropout
 
 model = Sequential()
-model.add(Convolution2D(32,3,3,input_shape= (64,64,3),activation = 'relu'))
+model.add(Convolution2D(32,3,3,input_shape= (128,128,3),activation = 'relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Convolution2D(64,3,3,activation = 'relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Convolution2D(64,3,3,activation = 'relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Convolution2D(32,3,3,activation = 'relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+
 model.add(Flatten())
 
 model.add(Dense(output_dim=128, activation = 'relu'))
+model.add(Dense(output_dim=128, activation = 'relu'))
+model.add(Dropout(0.1))
 model.add(Dense(output_dim=1, activation = 'sigmoid'))
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy',metrics=['accuracy'])
 
@@ -24,21 +34,39 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory(
         'dataset/training_set',
-        target_size=(64, 64),
+        target_size=(128, 128),
         batch_size=32,
         class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(
         'dataset/test_set',
-        target_size=(64, 64),
+        target_size=(128, 128),
         batch_size=32,
         class_mode='binary')
 #%%
 model.fit_generator(
         training_set,
-        steps_per_epoch=8000,
+        steps_per_epoch=8000/32,
         epochs=25,
         validation_data=test_set,
-        validation_steps=2000)
+        validation_steps=2000/32)
+#%%Save model
+model.save('my_model.h5')
+#%%LOAD MODEL
+from keras.models import load_model
+mdl = load_model('my_model.h5')
+#%% PREDICTION
+import numpy as np
+from keras_preprocessing import image
+predimage =  image.load_img('dataset/single_prediction/asdasd.jpg',target_size=(128,128))
+predimage = image.img_to_array(predimage)
+predimage = np.expand_dims(predimage,axis = 0)
+result = model.predict(predimage)
+
+if result[0][0] == 1:
+        prediction = 'dog'
+else:
+        prediction = 'cat'
+print(prediction)
 
 #%%
